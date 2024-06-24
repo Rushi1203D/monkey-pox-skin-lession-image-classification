f752cc521bc09f2efb4efeb421dfe96e8ab9db50
Rushi1203D <121380520+Rushi1203D@users.noreply.github.com>
2024-06-24 10:07:48 +0530

    Create inceptionv3_model.py

---
+++ inceptionv3_model.py
@@ -0,0 +1,108 @@
+InceptionV3
+
+import tensorflow as tf
+from tensorflow import keras
+from keras.applications import resnet
+from keras.layers import Flatten , Dense , Activation
+from keras import optimizers , Sequential
+from keras.optimizers import Adam
+from keras import models , layers
+from keras.layers import Input
+from keras.models import Model
+from sklearn.metrics import accuracy_score, confusion_matrix
+import seaborn as sns
+import numpy as np
+import itertools
+import matplotlib.pyplot as plt
+
+from google.colab import drive
+drive.mount("/content/gdrive")
+
+train_path='/content/gdrive/MyDrive/Dataset/train'
+val_path='/content/gdrive/MyDrive/Dataset/val'
+test_data='/content/gdrive/MyDrive/Dataset/test'
+
+from keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale=1./255,zoom_range=0.15,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.15)
+val_datagen = ImageDataGenerator(rescale=1./255)
+testdata_datagen=ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(train_path,target_size=(224, 224),batch_size=64,shuffle=True,class_mode='sparse')
+val_generator = val_datagen.flow_from_directory(val_path,target_size=(224,224),batch_size=64,shuffle=False,class_mode='sparse')
+test_generator = testdata_datagen.flow_from_directory(test_data,target_size=(224,224),batch_size=64,shuffle=False,class_mode='sparse')
+
+inc_model = tf.keras.applications.InceptionV3(pooling='max',
+                    weights='imagenet',
+                    include_top=False,
+                    input_shape=(224,224,3))
+for layers in inc_model.layers:
+            layers.trainable=False
+
+from keras.layers import Input,BatchNormalization , Dropout
+
+model = Sequential()
+model.add(inc_model)
+model.add(BatchNormalization())
+model.add(Flatten())
+model.add(Dense(256,activation='relu'))
+model.add(Dense(128,activation='relu'))
+model.add(Dense(4,activation='softmax'))
+model.summary()
+
+model.compile(optimizer=Adam(lr=0.0001),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(train_generator,
+                    validation_data=val_generator,
+                    epochs=100)
+
+model.evaluate(test_generator)
+
+Y_pred = model.predict(test_generator)
+
+y_pred = np.argmax(Y_pred, axis=1)
+
+y_true = test_generator.classes
+
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+cm = confusion_matrix(y_true, y_pred)
+
+import pandas as pd
+cm_df = pd.DataFrame(cm,
+                     index = ['CP','ME','MP','NL'],
+                     columns = ['CP','ME','MP','NL'])
+
+import keras
+from matplotlib import pyplot as plt
+
+plt.subplots(figsize=(11.5,2.5))
+plt.subplot(1,3,1)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('InceptionV3 Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Number of Epochs')
+plt.legend(['Train_Acc', 'Val_Acc'], loc='lower right')
+
+plt.subplot(1,3,2)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('InceptionV3 Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Number of Epochs')
+plt.legend(['Train_Loss', 'Val_Loss'], loc='upper right')
+
+plt.subplot(1,3,3)
+#plt.figure(figsize=(3.5,2.5))
+colormap = sns.color_palette("Greens")
+sns.heatmap(cm_df,cmap=colormap, annot=True)
+plt.title('InceptionV3 Confusion Matrix')
+plt.ylabel('True Labels')
+plt.xlabel('Predicted Labels')
+plt.show()
+
+print(classification_report(y_true, y_pred))
